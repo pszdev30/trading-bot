be47eb344ce669d63a1493bbf953f37e6699fee9
pszdev30 <pszjukalkar30@gmail.com>
2021-01-08 21:56:39 -0500

    wrote API response to output file

--- ta.py
+++ ta.py
@@ -9,9 +9,10 @@ import requests
 api = alpaca.REST(APCA_API_KEY_ID, APCA_API_SECRET_KEY,
                   APCA_API_PORTFOLIO_BASE_URL)
 
+
 timeframe = '1D'
 symbols = ','.join(screened_tickers)
-limit = 1
+limit = 200
 
 DAY_BAR_URL = APCA_DATA_BARS_URL + \
     '/{}?symbols={}&limit={}'.format(timeframe, symbols, limit)
@@ -19,4 +20,7 @@ DAY_BAR_URL = APCA_DATA_BARS_URL + \
 
 r = requests.get(DAY_BAR_URL, headers=HEADERS)
 
-print(json.dumps(r.json(), indent=4))
+data = json.dumps(r.json(), indent=4)
+
+with open('output.txt', 'w') as output:
+    output.write(data)
