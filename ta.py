1bea86b96d196365e44d7f351c39e21cf51cf1dc
pszdev30 <pszjukalkar30@gmail.com>
2021-01-09 16:37:10 -0500

    calculated basic MA indicators

--- ta.py
+++ ta.py
@@ -1,3 +1,4 @@
+import btalib
 import pandas as pd
 import json
 import requests
@@ -6,9 +7,28 @@ from secrets import *
 from globals import *
 
 
-def two_hundred_ema():
+def calc_indicators(ticker):
+    df = pd.read_csv('bta-data/{}.txt'.format(ticker),
+                     parse_dates=True, index_col='Date')
+
+    return two_hundred_ema(df), two_hundred_sma(df)
+
+
+def two_hundred_sma(df):
+    sma = btalib.sma(df, period=200).df
+    return sma
+
+
+def two_hundred_ema(df):
+    ema = btalib.ema(df, period=200).df
+    return ema
+
+
+def macd(ticker):
     pass
 
 
-def macd():
-    pass
+z = calc_indicators('MSFT')
+# g = calc_indicators('MSFT')
+
+print(z)
