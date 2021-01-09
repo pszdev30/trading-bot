8dc81a4d0e26107074667ed0a8d1aa7cb5f02e9d
pszdev30 <pszjukalkar30@gmail.com>
2021-01-08 21:31:18 -0500

    setup bars endpoint to retrieve data for all screened tickers

--- ta.py
+++ ta.py
@@ -2,10 +2,21 @@ import alpaca_trade_api as alpaca
 import pandas as pd
 import json
 from secrets import *
+from globals import *
+import requests
 
 
-api = alpaca.REST(APCA_API_KEY, APCA_SECRET_KEY, APCA_API_BASE_URL)
+api = alpaca.REST(APCA_API_KEY_ID, APCA_API_SECRET_KEY,
+                  APCA_API_PORTFOLIO_BASE_URL)
+
+timeframe = '1D'
+symbols = ','.join(screened_tickers)
+limit = 1
+
+DAY_BAR_URL = APCA_DATA_BARS_URL + \
+    '/{}?symbols={}&limit={}'.format(timeframe, symbols, limit)
 
 
-def ingest_stream(minute_bar):
-    print(minute_bar)
+r = requests.get(DAY_BAR_URL, headers=HEADERS)
+
+print(json.dumps(r.json(), indent=4))
