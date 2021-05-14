71d3b56d4c9d4f7399f2e2f044438361f295dc4d
pszdev30 <pszjukalkar30@gmail.com>
2021-05-13 20:36:39 -0400

    separated backtrader testing

--- backtrader/backtester.py
+++ backtrader/backtester.py
@@ -1,7 +1,13 @@
 import backtrader as bt
+import pandas as pd
 
 cerebro = bt.Cerebro()
 cerebro.broker.set_cash(100000)
+
+data = pd.read_csv('AAPL.csv', index_col='Date', parse_dates=True)
+feed = bt.feeds.PandasData(dataname=data)
+cerebro.adddata(feed)
+
 
 print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
