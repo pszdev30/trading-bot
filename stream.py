e81420b01ef97c7cd4db591e7c13a18dd24e0135
pszdev30 <pszjukalkar30@gmail.com>
2021-05-14 22:42:53 -0400

    fixed websocket and added thread to stop socket at market close

--- stream.py
+++ stream.py
@@ -11,26 +11,62 @@ import logging
 import threading
 import time
 import schedule
+import secrets
+from alpaca_trade_api.rest import REST
 from alpaca_trade_api.stream import Stream
 
+api = REST(secrets.APCA_API_KEY_ID, secrets.APCA_API_SECRET_KEY,
+           secrets.APCA_API_PORTFOLIO_BASE_URL)
 
 # Alpaca API config stuff
 
-async def bar_callback(minute_bar):
-    print('bar', minute_bar)
-    create_signal.ingest_stream(minute_bar)
+AUTH = {
+    "action": "auth",
+    "key": APCA_API_KEY_ID,
+    "secret": APCA_API_SECRET_KEY
+}
+
+LISTEN = {
+    "action": "subscribe",
+    "bars": screened_tickers
+}
 
 
-schedule.every().day.at('9:15').do(start_job())
+def on_open(ws):
+    ws.send(json.dumps(AUTH))
+    ws.send(json.dumps(LISTEN))
 
 
-def start_job():
-    # Initiate Class Instance
-    stream = Stream(APCA_API_KEY_ID,
-                    APCA_API_SECRET_KEY,
-                    base_url=URL(APCA_API_PORTFOLIO_BASE_URL),
-                    data_feed='iex')  # <- replace to SIP if you have PRO subscription
+def on_message(ws, bar):
+    create_signal.ingest_stream(bar)
+    # ws.close()
 
-    # subscribing to event
-    stream.subscribe_bars(bar_callback, 'PYPL')
-    stream.run()
+
+def on_close(ws):
+    print('closed :)')
+
+
+def on_error(ws, error_msg):
+    print(error_msg)
+
+
+def get_time_to_market_close():
+    clock = api.get_clock()
+    return (clock.next_close - clock.timestamp).total_seconds()
+
+
+def start_socket_connection():
+    websocket.enableTrace(True)
+    socket = secrets.APCA_WEB_SOCKET
+    ws = websocket.WebSocketApp(socket, on_open=on_open,
+                                on_message=on_message, on_close=on_close, on_error=on_error)
+
+    thread = threading.Thread(target=ws.run_forever)
+    thread.start()
+    time.sleep(round(get_time_to_market_close()) + 300)
+    ws.keep_running = False
+    thread.join()
+    print('Trading closed for the day :)')
+
+
+# start_socket_connection()
