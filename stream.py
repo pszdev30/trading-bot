2585ea49ce62d7b94f9e2e07ee00540fb0658ca3
pszdev30 <pszjukalkar30@gmail.com>
2021-05-25 15:44:41 -0400

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,18 +1,12 @@
-from alpaca_trade_api import StreamConn
+from alpaca_trade_api.rest import REST
 from alpaca_trade_api.common import URL
 import websocket
 import json
-import pandas as pd
-import globals
-import create_signal
-import asyncio
-import logging
 import threading
 import time
-import schedule
 import secrets
-from alpaca_trade_api.rest import REST
-from alpaca_trade_api.stream import Stream
+import globals
+import boto3
 
 api = REST(secrets.APCA_API_KEY_ID, secrets.APCA_API_SECRET_KEY,
            secrets.APCA_API_PORTFOLIO_BASE_URL)
@@ -36,8 +30,26 @@ def on_open(ws):
     ws.send(json.dumps(LISTEN))
 
 
-def on_message(ws, bar):
-    create_signal.ingest_stream(bar)
+def on_message(ws, minute_bar):
+    region = 'us-east-2'
+    instance = 'i-072a90a706d9e42d9'
+    # command = 'python3 trading-bot-scripts/create_signal.py {minute_bar}'.format(
+    #     minute_bar=minute_bar)
+    # trade_ec2 = boto3.client('ssm', region_name=region)
+    # trade_ec2.send_command(
+    #     InstanceIds=[
+    #         instance
+    #     ],
+    #     Comment='Triggers trading-bot-main',
+    #     DocumentName='AWS-RunShellScript',
+    #     Parameters={
+    #         'commands': [
+    #             command
+    #         ]
+    #     }
+    # )
+    # print(minute_bar)
+    # create_signal.start(minute_bar)
 
 
 def on_close(ws):
@@ -54,17 +66,18 @@ def get_time_to_market_close():
 
 
 def start_socket_connection():
-    websocket.enableTrace(True)
+    # websocket.enableTrace(True)
     socket = secrets.APCA_WEB_SOCKET
     ws = websocket.WebSocketApp(socket, on_open=on_open,
                                 on_message=on_message, on_close=on_close, on_error=on_error)
+    ws.run_forever()
 
-    thread = threading.Thread(target=ws.run_forever)
-    thread.start()
-    time.sleep(round(get_time_to_market_close()) + 300)
-    ws.keep_running = False
-    thread.join()
-    print('Trading closed for the day :)')
+    # thread = threading.Thread(target=ws.run_forever)
+    # thread.start()
+    # time.sleep(round(get_time_to_market_close()) + 300)
+    # ws.keep_running = False
+    # thread.join()
+    # print('Trading closed for the day :)')
 
 
-# start_socket_connection()
+start_socket_connection()
