e2bb75c034f28c0ab60e909e1822eec54e1e02e7
pszdev30 <pszjukalkar30@gmail.com>
2021-05-27 00:50:49 -0400

    refactored to use AWS secrets + s3 bucket

--- pipeline.py
+++ pipeline.py
@@ -1,29 +1,45 @@
 # import alpaca_trade_api as alpaca
-from alpaca_trade_api.rest import REST, TimeFrame
-from secrets import *
+from alpaca_trade_api.rest import REST
 from datetime import datetime, timedelta
-import globals
 import pandas as pd
 import requests
 import json
-import redis
+import boto3
+import aws_secrets
 
+s3_client = boto3.client('s3')
+dynamodb = boto3.resource('dynamodb')
+secrets = aws_secrets.get_secrets()
 
-api = REST(APCA_API_KEY_ID, APCA_API_SECRET_KEY,
-           APCA_API_PORTFOLIO_BASE_URL)
+APCA_API_KEY_ID = secrets["APCA-API-KEY-ID"]
+APCA_API_SECRET_KEY = secrets["APCA-API-SECRET-KEY"]
+APCA_API_PORTFOLIO_BASE_URL = secrets["APCA-API-PORTFOLIO-BASE-URL"]
+APCA_DATA_BARS_URL = secrets["APCA-DATA-BARS-V1-URL"]
+HEADERS = json.loads(secrets["HEADERS"])
+HEADERS['APCA-API-KEY-ID'] = APCA_API_KEY_ID
+HEADERS['APCA-API-SECRET-KEY'] = APCA_API_SECRET_KEY
+
+api = REST(APCA_API_KEY_ID, APCA_API_SECRET_KEY, APCA_API_PORTFOLIO_BASE_URL)
+
+screened_tickers_byte = s3_client.get_object(Bucket='trading-bot-s3',
+                                             Key='screened_tickers.json')
+screened_tickers_str = screened_tickers_byte.get()['Body'].read().decode(
+    'UTF-8')
+screened_tickers_list = (screened_tickers_str).split(',')
 
 
 def get_screened_ticker_data():
     two_hundred_days_ago = (datetime.now() - timedelta(days=200)).date()
-    start = pd.Timestamp(two_hundred_days_ago, tz=globals.NY).isoformat()
-    end = 'now'
-    timeframe = '1D'
+    start = pd.Timestamp(two_hundred_days_ago,
+                         tz="America/New_York").isoformat()
+    end = "now"
+    timeframe = "1D"
 
-    symbols = ','.join(globals.screened_tickers)
+    symbols = screened_tickers_str
     limit = 200
 
-    DAY_BAR_URL = APCA_DATA_BARS_URL + \
-        '/{}?symbols={}&limit={}'.format(timeframe, symbols, limit)
+    DAY_BAR_URL = APCA_DATA_BARS_URL + "/{}?symbols={}&limit={}".format(
+        timeframe, symbols, limit)
 
     r = requests.get(DAY_BAR_URL, headers=HEADERS)
     data = r.json()
@@ -33,21 +49,20 @@ def get_screened_ticker_data():
 
 def transform():
     data = get_screened_ticker_data()
-    for ticker in data:
-        file = 'bta-data/{}.txt'.format(ticker)
-        f = open(file, 'w+')
-        f.write('Date,Open,High,Low,Close,Volume,OpenInterest\n')
 
-        for bar in data[ticker]:
-            time = datetime.fromtimestamp(bar['t'])
-            date = time.strftime('%Y-%m-%d')
-            day_entry = '{},{},{},{},{},{},{}\n'.format(
-                date, bar['o'], bar['h'], bar['l'], bar['c'], bar['v'], 0.00)
-            f.write(day_entry)
+
+def push_to_s3(file, ticker):
+    # try:
+    #     response = s3_client.upload_file(
+    #         file, 'trading-bot-s3',
+    #         '/bta-data/{ticker}.txt'.format(ticker=ticker))
+    # except ClientError as e:
+    #     logging.error(e)
+    #     return False
+    return True
 
 
 transform()
-
 
 # with open('output.txt', 'w') as output:
 #     output.write(data)
