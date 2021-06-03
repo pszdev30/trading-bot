f3d560eff741abe34551fde4c0bfe09bbac0618c
pszdev30 <pszjukalkar30@gmail.com>
2021-06-03 01:33:50 -0400

    script to get secrets from AWS secrets manager

--- aws_secrets.py
+++ aws_secrets.py
@@ -1,11 +1,7 @@
-# Use this code snippet in your app.
-# If you need more information about configurations or implementing the sample code, visit the AWS docs:
-# https://aws.amazon.com/developers/getting-started/python/
-
-import boto3
+import json
 import base64
 from botocore.exceptions import ClientError
-import json
+import boto3
 
 
 def get_secrets():
@@ -16,7 +12,6 @@ def get_secrets():
     session = boto3.session.Session()
     client = session.client(service_name='secretsmanager',
                             region_name=region_name)
-
     try:
         get_secret_value_response = client.get_secret_value(
             SecretId=secret_name)
@@ -33,14 +28,11 @@ def get_secrets():
             raise e
     else:
         if 'SecretString' in get_secret_value_response:
-            print(get_secret_value_response['SecretString'])
             secret = json.loads(get_secret_value_response['SecretString'])
             return secret
         else:
             decoded_binary_secret = base64.b64decode(
                 get_secret_value_response['SecretBinary'])
-
-    # Your code goes here.
 
 
 # print(get_secrets())
